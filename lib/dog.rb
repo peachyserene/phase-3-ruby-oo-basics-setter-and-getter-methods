1417ad04af9edfc2006d2b9979cee2713a11d7bb
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-06-12 21:43:07 -0400

    commit tree ed2488bbc4855206227e215d32169947224a2a8b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+def name= (name_param)
+    @name = name_param
+end
 
+def name
+    @name
+end
+
+def breed= (breed_param)
+    @breed = breed_param
+end
+
+def breed
+    @breed
+end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+    # setter
+def name= (name_param)
+    @name = name_param
+end
+# getter
+def name
+    @name
+end
 
+#setter
+def job= (job_param)
+    @job = job_param
+end
+
+def job
+    @job
+end
 end
